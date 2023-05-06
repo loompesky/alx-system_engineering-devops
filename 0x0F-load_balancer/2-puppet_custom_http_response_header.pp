a075963e853811634752ac081d7996b682e63015
loompesky <loompesky@yahoo.com>
2023-05-06 07:11:30 -0700

    Load Balancer

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,19 @@
+# use puppet to automate creating HTTP header response
+
+exec {'apt_update':
+  command => '/usr/bin/apt-get update',
+}
+
+-> package { 'nginx':
+  ensure => 'present',
+}
+
+-> file_line { 'header':
+  path  => '/etc/nginx/nginx.conf',
+  match => 'http {',
+  line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
+}
+
+-> exec { 'run':
+  command => '/usr/sbin/service nginx restart',
+}
